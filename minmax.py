e5ca3fb9168a0a1feb1c810e2b5a37071f341660
Pablo del Mazo <delmazosevillanopablo@gmail.com>
2019-12-18 21:47:25 +0100

    Alpha-beta minmax algorithm for tictactoe

--- minmax.py
+++ minmax.py
@@ -1,23 +1,82 @@
 import numpy as np
 
 class MinMaxAgent:
-	def __init__(self, board_class, playerID=1):
+	def __init__(self, board_class, playerID=1,ab=True):
 		'''
 		If playerID = 1 agent tries to maximize score, 
 		else to minimize
+		ab: Use alpha-beta to optimize tree exploration.
 		'''
 		self.board = board_class
 		self.playerID = playerID
+		self.ab = ab
 
 	def select_action(self, state, avail_act):
-		if self.playerID == 1:
-			action, _ = self.maxfun(state)
+		if self.ab:
+			alpha = -100000000
+			beta = 100000000
+			if self.playerID == 1:
+				action, _ = self.maxfunAB(state, alpha, beta)
+			else:
+				action, _ = self.minfunAB(state, alpha, beta)
+			return action
 		else:
-			action, _ = self.minfun(state)
-
-		return action
+			if self.playerID == 1:
+				action, _ = self.maxfun(state)
+			else:
+				action, _ = self.minfun(state)
+			return action
 
 	def maxfun(self, state):
+		turn = self.board.turn
+		best_reward = -100000000
+		final_action = None
+
+		# Iterate over possible actions:
+		for action in self.board.avail_actions(state):
+			index = self.board.action2index(action)
+			next_state, reward = self.board.play(index[0], index[1])
+			if not self.board.end:
+				_, reward = self.minfun(next_state)
+
+			# If reward is higher replace final_action.
+			if reward > best_reward:
+				final_action = action
+				best_reward = reward
+
+			# Reset state
+			self.board.turn = turn
+			self.board.board[index[0], index[1]] = 0
+			self.board.end = False
+
+		return final_action, best_reward
+
+	def minfun(self, state):
+		turn = self.board.turn
+		best_reward = +100000000
+		final_action = None
+
+		# Iterate over possible actions:
+		for action in self.board.avail_actions(state):
+			index = self.board.action2index(action)
+			next_state, reward = self.board.play(index[0], index[1])
+
+			if not self.board.end:
+				_, reward = self.maxfun(next_state)
+
+			# If reward is higher replace final_action.
+			if reward < best_reward:
+				final_action = action
+				best_reward = reward
+
+			# Reset state
+			self.board.turn = turn
+			self.board.board[index[0], index[1]] = 0
+			self.board.end = False
+
+		return final_action, best_reward
+
+	def maxfunAB(self, state, alpha, beta):
 		#print('state:',state)
 		turn = self.board.turn
 		best_reward = -100000000
@@ -27,25 +86,28 @@ class MinMaxAgent:
 		for action in self.board.avail_actions(state):
 			index = self.board.action2index(action)
 			next_state, reward = self.board.play(index[0], index[1])
-			#print('next_state:',next_state)
+
 			if not self.board.end:
-				_, reward = self.minfun(next_state)
+				_, reward = self.minfunAB(next_state, alpha, beta)
+			
+			alpha = max(reward, alpha)
 
 			# If reward is higher replace final_action.
 			if reward > best_reward:
 				final_action = action
 				best_reward = reward
 
-			#print(self.board.end)
 			# Reset state
 			self.board.turn = turn
 			self.board.board[index[0], index[1]] = 0
 			self.board.end = False
 
+			if alpha > beta:
+				break
+
 		return final_action, best_reward
 
-	def minfun(self, state):
-		#print('state:',state)
+	def minfunAB(self, state, alpha, beta):
 		turn = self.board.turn
 		best_reward = +100000000
 		final_action = None
@@ -54,20 +116,24 @@ class MinMaxAgent:
 		for action in self.board.avail_actions(state):
 			index = self.board.action2index(action)
 			next_state, reward = self.board.play(index[0], index[1])
-			#print('next_state:',next_state)
+
 			if not self.board.end:
-				_, reward = self.maxfun(next_state)
+				_, reward= self.maxfunAB(next_state, alpha, beta)
+			
+			beta = min(reward, beta)
 
 			# If reward is higher replace final_action.
 			if reward < best_reward:
 				final_action = action
 				best_reward = reward
 
-			#print(self.board.end)
 			# Reset state
 			self.board.turn = turn
 			self.board.board[index[0], index[1]] = 0
 			self.board.end = False
+
+			if alpha > beta:
+				break
 
 		return final_action, best_reward
 
